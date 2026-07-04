2a47e58d4df6d1c54f151d450bb7d538fa79431d
fabienmartrelabevent <fabien@lab-event.com>
2026-07-04 11:50:44 +0200

    Update recette.spec.ts

--- lab-event-mobile/lab-event-mobile/tests/recette.spec.ts
+++ lab-event-mobile/lab-event-mobile/tests/recette.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect, devices } from '@playwright/test';
 
+// Le robot utilise un écran d'iPhone 13
 test.use({ ...devices['iPhone 13'] });
 
-test('Vérification de la page d\'accueil Lab Event', async ({ page }) => {
+test('Vérifier que l\'application bloque les connexions vides', async ({ page }) => {
+  // 1. Le robot ouvre ton application mobile
   await page.goto('https://lab-event-mobile.vercel.app/');
-  await expect(page).not.toHaveTitle(/404/);
-  await page.waitForTimeout(2000); 
+
+  // 2. Il clique directement sur le bouton "Se connecter"
+  await page.click('button:has-text("Se connecter")');
+
+  // 3. Le QA vérifie que le message d'erreur attendu est bien visible à l'écran
+  const messageErreur = page.locator('text=Sous-domaine et token requis.');
+  await expect(messageErreur).toBeVisible();
 });
