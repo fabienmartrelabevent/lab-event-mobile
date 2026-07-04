d6b94ddf65974ab0cb50dc504d7bcd7647435365
fabienmartrelabevent <fabien@lab-event.com>
2026-07-04 11:57:18 +0200

    Update recette.spec.ts

--- lab-event-mobile/lab-event-mobile/tests/recette.spec.ts
+++ lab-event-mobile/lab-event-mobile/tests/recette.spec.ts
@@ -1,16 +1,25 @@
 import { test, expect, devices } from '@playwright/test';
 
-// Le robot utilise un écran d'iPhone 13
 test.use({ ...devices['iPhone 13'] });
 
-test('Vérifier que l\'application bloque les connexions vides', async ({ page }) => {
-  // 1. Le robot ouvre ton application mobile
+test('Connexion sécurisée réussie et accès à l\'application', async ({ page }) => {
+  // Le robot récupère les informations cachées
+  const monSousDomaine = process.env.PLAYWRIGHT_SUBDOMAIN || ''; 
+  const monToken = process.env.PLAYWRIGHT_TOKEN || '';
+
+  // 1. Il ouvre le site
   await page.goto('https://lab-event-mobile.vercel.app/');
 
-  // 2. Il clique directement sur le bouton "Se connecter"
+  // 2. Il remplit les deux cases automatiquement
+  await page.locator('input').nth(0).fill(monSousDomaine);
+  await page.locator('input').nth(1).fill(monToken);
+
+  // 3. Il clique sur "Se connecter"
   await page.click('button:has-text("Se connecter")');
 
-  // 3. Le QA vérifie que le message d'erreur attendu est bien visible à l'écran
-  const messageErreur = page.locator('text=Sous-domaine et token requis.');
-  await expect(messageErreur).toBeVisible();
+  // 4. On lui laisse 4 secondes pour que l'application valide et charge l'écran suivant
+  await page.waitForTimeout(4000); 
+
+  // 5. VÉRIFICATION : Si la connexion a réussi, le bouton "Se connecter" n'existe plus à l'écran
+  await expect(page.locator('button:has-text("Se connecter")')).not.toBeVisible();
 });
